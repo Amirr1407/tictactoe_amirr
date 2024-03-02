632e0f31503e055343cd79883b31795dccb3b576
Amirr1407 <92157140+Amirr1407@users.noreply.github.com>
2024-03-02 18:31:41 +0530

    Update and rename Code.c to Game.java

---
+++ Game.java
@@ -0,0 +1,114 @@
+/*
+Author:- Amirthavarshini
+*/
+import java.util.Scanner;
+
+public class Game{
+    private static char[][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        boolean gameInProgress = true;
+
+        while (gameInProgress) {
+            printBoard();
+            makeMove();
+            gameInProgress = !isGameOver();
+            switchPlayer();
+        }
+
+        printBoard();
+        displayResult();
+    }
+
+    private static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private static void makeMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        do {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+        } while (!isValidMove(row, col));
+
+        board[row][col] = currentPlayer;
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            System.out.println("Invalid move. Try again.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isGameOver() {
+        return hasPlayerWon() || isBoardFull();
+    }
+
+    private static boolean hasPlayerWon() {
+        return checkRows() || checkColumns() || checkDiagonals();
+    }
+
+    private static boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals() {
+        return (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+               (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer);
+    }
+
+    private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void displayResult() {
+        if (hasPlayerWon()) {
+            System.out.println("Player " + currentPlayer + " wins!");
+        } else {
+            System.out.println("It's a draw!");
+        }
+    }
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+}
